3c0d96fb12e5da7eada17b2208f70f0efaca610c
Michał Papierski <michal@papierski.net>
2019-03-02 17:13:00 +0100

    Crawl submitted requests.

--- pjatk.py
+++ pjatk.py
@@ -4,6 +4,10 @@ import requests
 import re
 import json
 import sys
+import operator
+from jinja2 import Template
+from datetime import datetime
+import pickle
 from bs4 import BeautifulSoup
 
 re_postback = re.compile(
@@ -12,7 +16,7 @@ re_postback = re.compile(
 message_title = u'Pojawiła się nowa ocena w dziekanacie PJWSTK'
 
 
-def send_message(title, text, api_key=None, mailgun_domain=None, send_from=None, send_to=None):
+def send_message(title, text, api_key=None, mailgun_domain=None, send_from=None, send_to=None, html=None):
     assert api_key is not None
     assert mailgun_domain is not None
     assert send_from is not None
@@ -21,13 +25,16 @@ def send_message(title, text, api_key=None, mailgun_domain=None, send_from=None,
     click.echo('Send message')
     click.echo(text)
     click.echo('---')
+    data = {"from": send_from,
+            "to": [send_to],
+            "subject": title,
+            "text": text}
+    if html is not None:
+        data['html'] = html
     r = requests.post(
         "https://api.mailgun.net/v3/{}/messages".format(mailgun_domain),
         auth=("api", api_key),
-        data={"from": send_from,
-              "to": [send_to],
-              "subject": title,
-              "text": text})
+        data=data)
     r.raise_for_status()
 
 
@@ -50,6 +57,9 @@ def get_form_events(bs):
 
     return event
 
+@click.group()
+def cli():
+    pass
 
 @click.command()
 @click.option('--login', required=True)
@@ -58,7 +68,7 @@ def get_form_events(bs):
 @click.option('--send-from', required=True)
 @click.option('--send-to', required=True)
 @click.option('--mailgun-domain', required=True)
-def pjatk(login, password, api_key, send_from, send_to, mailgun_domain):
+def oceny(login, password, api_key, send_from, send_to, mailgun_domain):
     s = requests.Session()
     # Get base values for form
     r = s.get('https://dziekanat.pjwstk.edu.pl/Login.aspx')
@@ -133,5 +143,123 @@ def pjatk(login, password, api_key, send_from, send_to, mailgun_domain):
     r.raise_for_status()
 
 
+@click.command()
+@click.option('--login', required=True)
+@click.option('--password', required=True)
+@click.option('--api-key', required=True)
+@click.option('--send-from', required=True)
+@click.option('--send-to', required=True)
+@click.option('--mailgun-domain', required=True)
+def podania(login, password, api_key, send_from, send_to, mailgun_domain):
+    s = requests.Session()
+    # Get base values for form
+    r = s.get('https://podania.pjwstk.edu.pl/login.aspx')
+    r.raise_for_status()
+    bs = BeautifulSoup(r.content, 'html.parser')
+    form = get_form_events(bs)
+    form['__EVENTTARGET'] = ''
+    form['__EVENTARGUMENT'] = ''
+    form['Button1'] = 'Zaloguj'
+    form['TextBox1'] = login
+    form['TextBox2'] = password
+    r = s.post('https://podania.pjwstk.edu.pl/login.aspx', data=form)
+    r.raise_for_status()
+    bs = BeautifulSoup(r.content, 'html.parser')
+    hello = bs.select("#ContentPlaceHolder1_Label1")[0]
+    print(hello.text)
+
+    r = s.get('https://podania.pjwstk.edu.pl/wyslane.aspx')
+    r.raise_for_status()
+    bs = BeautifulSoup(r.content, 'html.parser')
+    table = bs.select("#ContentPlaceHolder1_GridView2")[0]
+
+    podania = []
+
+    for row in table.select("tr"):
+        cols = list(row.select("td"))
+        if not cols:
+            continue
+        student = cols[0].text.strip()
+        url = cols[0].select("a")[0]['href']
+        wys, = re.search(r'^podanie\.aspx\?wys=(\d+)$', url).groups()
+        wys = int(wys)
+        typ_podania = cols[1].text.strip()
+        status = cols[2].text.strip()
+        data_zlozenia = cols[3].text.strip()
+        data_zlozenia = datetime.strptime(data_zlozenia, '%d.%m.%Y %H:%M:%S')
+        r = s.get('https://podania.pjwstk.edu.pl/{}'.format(url))
+        r.raise_for_status()
+        bs = BeautifulSoup(r.content, 'html.parser')
+        # Get details
+        wnioskodawca = bs.select('#ContentPlaceHolder1_lblStud')[0]
+        for br in wnioskodawca.find_all("br"):
+            br.replace_with("\n")
+        wnioskodawca = wnioskodawca.text.strip()
+        uzasadnienie = bs.select('#ContentPlaceHolder1_uzasad')[0].text.strip()
+
+        historia = []
+
+        for i, row_historia in enumerate(bs.select('#ContentPlaceHolder1_GridView1')[0].select("tr")):
+            cols = list(row_historia.select("td"))
+            if not cols:
+                continue
+            miejsce_podania = cols[0].text.strip()
+            data = cols[1].text.strip()
+            status = cols[2].text.strip()
+            informacja = cols[3].text.strip()
+            
+            data = {
+                'miejsce_podania': miejsce_podania,
+                'data': datetime.strptime(data, '%d.%m.%Y %H:%M:%S') if data else None,
+                'status': status,
+                'informacja': informacja,
+            }
+            historia.append(data)
+
+        podania.append({
+            'student': student,
+            'url': url,
+            'wys': wys,
+            'typ_podania': typ_podania,
+            'data_zlozenia': data_zlozenia,
+            'wnioskodawca': wnioskodawca,
+            'historia': historia,  
+            'uzasadnienie': uzasadnienie,
+        })
+
+    podania.sort(key=operator.itemgetter('data_zlozenia'), reverse=True)
+
+
+    def default(o):
+        if isinstance(o, (datetime,)):
+            return o.isoformat()
+
+    with open('podania.json', 'w') as podania_json:
+        json.dump(podania, podania_json, sort_keys=True, indent=4, default=default)
+    
+    try:
+        with open('podania_state.pickle', 'rb') as podania_pickle:
+            # pickle because it contains datetime objects
+            current_podania = pickle.load(podania_pickle)
+    except IOError:
+        current_podania = []
+    if podania != current_podania:
+        with open('podania.html') as tpl:
+            template = Template(tpl.read())
+            content = template.render(podania=podania)
+            print('Sending email...')
+            send_message('Zmiany w systemie podań', json.dumps(podania, sort_keys=True, indent=4, default=default),
+                     api_key=api_key, mailgun_domain=mailgun_domain,
+                     send_from=send_from, send_to=send_to, html=content)
+
+        with open('podania_state.pickle', 'wb') as podania_pickle:
+            pickle.dump(podania, podania_pickle)
+
+    r = s.get('https://podania.pjwstk.edu.pl/logout.aspx')
+    r.raise_for_status()
+
+
+cli.add_command(podania)
+cli.add_command(oceny)
 if __name__ == '__main__':
-    pjatk(auto_envvar_prefix='PJATK')
+    cli(auto_envvar_prefix='PJATK')
